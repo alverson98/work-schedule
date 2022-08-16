a7aebd327e16434ef04e46f91660c58b870eb9a7
Hannah Alverson <alverson98@yahoo.com>
2022-08-16 05:56:15 -0400

    deleted save button function to redo

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,22 @@ $("#currentDay").text(currentDay.format("MMM Do, YYYY"));
 
 // Current Time - 24hr
 var currentTime = moment().format("HH");
+console.log(currentTime);
 
 // Time Blocks Divs 8am-6pm
-var time8A = $("#8a");
-var time9A = $("#9a");
-var time10A = $("#10a");
-var time11A = $("#11a");
-var time12A = $("#12p");
-var time1P = $("#1p");
-var time2P = $("#2p");
-var time3P = $("#3p");
-var time4P = $("#4p");
-var time5P = $("#5p");
-var time6P = $("#6p");
+var time8A = $("#8");
+var time9A = $("#9");
+var time10A = $("#10");
+var time11A = $("#11");
+var time12A = $("#12");
+var time1P = $("#1");
+var time2P = $("#2");
+var time3P = $("#3");
+var time4P = $("#4");
+var time5P = $("#5");
+var time6P = $("#6");
 
-// Time Block Array
+// Time Block Div Array
 var timeBlockArray = [
   time8A,
   time9A,
@@ -36,38 +37,35 @@ var timeBlockArray = [
   time6P,
 ];
 
+
 // Save buttons
 var saveBtn = $(".saveBtn");
 console.log(saveBtn);
 
-function timeBlockStatus(timeBlockValues) {
-  // Assigning a value to all the time blocks
-  for (var i = 0; i < timeBlockArray.length; i++) {
-    var timeBlockValues = (timeBlockArray[i].value = 8 + i);
-    console.log(timeBlockValues);
+function schedule() {
+  function timeStatus() {
+    // Assigning a value to all the time blocks
+    for (var i = 0; i < timeBlockArray.length; i++) {
+      var timeBlockValues = (timeBlockArray[i].value = 8 + i);
+      console.log(timeBlockValues);
 
-    // Assigning time block class based on past, present, or future -- color coded
-    if (timeBlockValues == currentTime) {
-      $("textarea").addClass("present");
-    } else if (timeBlockValues < currentTime) {
-      $("textarea").addClass("past");
-    } else {
-      $("textarea").addClass("future");
+      // Assigning time block class based on past, present, or future -- color coded
+      if (timeBlockValues == currentTime) {
+        $("textarea").addClass("present");
+      } else if (timeBlockValues < currentTime) {
+        $("textarea").addClass("past");
+      } else {
+        $("textarea").addClass("future");
+      }
+
+      $(timeBlockArray[i].val(localStorage.getItem(timeBlockArray[i])));
     }
   }
+  timeStatus();
+
+  $(".saveBtn").click(function () {
+    
+  });
 }
 
-// Adding new event to time block
-function handleFormSubmit(event) {
-  event.preventDefault();
-
-  var selectBtnTarget = $(event.currentTarget);
-
-  var eventTextTarget = selectBtnTarget.siblings("textarea");
-
-  localStorage.setItem("eventText", eventTextTarget.val());
-}
-
-saveBtn.on("click", handleFormSubmit);
-
-timeBlockStatus();
+schedule();
